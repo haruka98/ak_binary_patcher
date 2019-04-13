ef5c15e7666432da250e4cd993528f71a04a6cfd
unknown <45176998+haruka98@users.noreply.github.com>
2019-04-13 14:06:28 +0200

    changed text output

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ int convert(FILE* fp_in, FILE* fp_out, int search[], int size, int replace[]);
 int main(int argc, char* argv[]) {
 	int pattern[] = {0x3D, 0x0A, 0x0A, 0x0A, 0x00, 0x0F, 0x84, 0xEB, 0xFE, 0xFF, 0xFF, 0x3D};
 	int replace_ip[4];
-	//user input
-	printf("Enter ip:\n");
+	//interface
+	printf("Enter new ip:\n");
 	scanf("%d.%d.%d.%d", &replace_ip[0], &replace_ip[1], &replace_ip[2], &replace_ip[3]);
-	printf("\n%x %x %x\n", replace_ip[0], replace_ip[1], replace_ip[2]);
+	printf("\nUsing %x %x %x\n", replace_ip[0], replace_ip[1], replace_ip[2]);
 	//GatewayServer
 	FILE* fp_gs_in = fopen("input/GatewayServer", "rb");
 	if(fp_gs_in != NULL) {
 		FILE* fp_gs_out = fopen("output/GatewayServer", "ab");
-		printf("%d", convert(fp_gs_in, fp_gs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in GatewayServer.\n", convert(fp_gs_in, fp_gs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_gs_out);
 	}
 	fclose(fp_gs_in);
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_hs_in = fopen("input/HTTPAServer", "rb");
 	if(fp_hs_in != NULL) {
 		FILE* fp_hs_out = fopen("output/HTTPAServer", "ab");
-		printf("%d", convert(fp_hs_in, fp_hs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in HTTPAServer.\n", convert(fp_hs_in, fp_hs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_hs_out);
 	}
 	fclose(fp_hs_in);
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ls_in = fopen("input/LoginServer", "rb");
 	if(fp_ls_in != NULL) {
 		FILE* fp_ls_out = fopen("output/LoginServer", "ab");
-		printf("%d", convert(fp_ls_in, fp_ls_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in LoginServer.\n", convert(fp_ls_in, fp_ls_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ls_out);
 	}
 	fclose(fp_ls_in);
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ms_in = fopen("input/MissionServer", "rb");
 	if(fp_ms_in != NULL) {
 		FILE* fp_ms_out = fopen("output/MissionServer", "ab");
-		printf("%d", convert(fp_ms_in, fp_ms_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in MissionServer.\n", convert(fp_ms_in, fp_ms_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ms_out);
 	}
 	fclose(fp_ms_in);
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ts_in = fopen("input/TicketServer", "rb");
 	if(fp_ts_in != NULL) {
 		FILE* fp_ts_out = fopen("output/TicketServer", "ab");
-		printf("%d", convert(fp_ts_in, fp_ts_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in TicketServer.\n", convert(fp_ts_in, fp_ts_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ts_out);
 	}
 	fclose(fp_ts_in);
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ws_in = fopen("input/WorldServer", "rb");
 	if(fp_ws_in != NULL) {
 		FILE* fp_ws_out = fopen("output/WorldServer", "ab");
-		printf("%d", convert(fp_ws_in, fp_ws_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in WorldServer.\n", convert(fp_ws_in, fp_ws_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ws_out);
 	}
 	fclose(fp_ws_in);
@@ -61,7 +61,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ws1_in = fopen("input/WorldServer101", "rb");
 	if(fp_ws1_in != NULL) {
 		FILE* fp_ws1_out = fopen("output/WorldServer101", "ab");
-		printf("%d", convert(fp_ws1_in, fp_ws1_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in WorldServer101.\n", convert(fp_ws1_in, fp_ws1_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ws1_out);
 	}
 	fclose(fp_ws1_in);
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_ws2_in = fopen("input/WorldServer102", "rb");
 	if(fp_ws2_in != NULL) {
 		FILE* fp_ws2_out = fopen("output/WorldServer102", "ab");
-		printf("%d", convert(fp_ws2_in, fp_ws2_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in WorldServer102.\n", convert(fp_ws2_in, fp_ws2_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_ws2_out);
 	}
 	fclose(fp_ws2_in);
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_zs_in = fopen("input/ZoneServer", "rb");
 	if(fp_zs_in != NULL) {
 		FILE* fp_zs_out = fopen("output/ZoneServer", "ab");
-		printf("%d", convert(fp_zs_in, fp_zs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in ZoneServer.\n", convert(fp_zs_in, fp_zs_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_zs_out);
 	}
 	fclose(fp_zs_in);
@@ -85,7 +85,7 @@ int main(int argc, char* argv[]) {
 	FILE* fp_zs1_in = fopen("input/ZoneServer101", "rb");
 	if(fp_zs1_in != NULL) {
 		FILE* fp_zs1_out = fopen("output/ZoneServer101", "ab");
-		printf("%d", convert(fp_zs1_in, fp_zs1_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in ZoneServer101.\n", convert(fp_zs1_in, fp_zs1_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_zs1_out);
 	}
 	fclose(fp_zs1_in);
@@ -93,10 +93,13 @@ int main(int argc, char* argv[]) {
 	FILE* fp_zs2_in = fopen("input/ZoneServer102", "rb");
 	if(fp_zs2_in != NULL) {
 		FILE* fp_zs2_out = fopen("output/ZoneServer102", "ab");
-		printf("%d", convert(fp_zs2_in, fp_zs2_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
+		printf("Found %d occurrences in ZoneServer102.\n", convert(fp_zs2_in, fp_zs2_out, pattern, sizeof(pattern) / sizeof(int), replace_ip));
 		fclose(fp_zs2_out);
 	}
 	fclose(fp_zs2_in);
+	printf("\nDone... Press any key to quit.");
+	fflush(stdin);
+	getchar();
 	return 0;
 }
 
